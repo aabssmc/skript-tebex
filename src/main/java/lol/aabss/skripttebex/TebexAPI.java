51260ab135ba116cc21823a3e4a74c985b87619a
aabssmc <lilmikeyninja203@gmail.com>
2023-11-19 11:57:18 -0800

    added body option in TebexAPI class

--- src/main/java/lol/aabss/skripttebex/TebexAPI.java
+++ src/main/java/lol/aabss/skripttebex/TebexAPI.java
@@ -1,12 +1,14 @@
+// https://docs.tebex.io/plugin/
+
 package lol.aabss.skripttebex;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -17,7 +19,7 @@ public class TebexAPI {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod(method);
         connection.setRequestProperty("X-Tebex-Secret", secret);
-        if (connection.getResponseCode() == 200){
+        if (connection.getResponseCode() == 200 || connection.getResponseCode() == 204 || connection.getResponseCode() == 201){
             InputStream stream = connection.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
             StringBuilder response = new StringBuilder();
@@ -30,4 +32,33 @@ public class TebexAPI {
         }
         return null;
     }
+
+    public static JsonObject api(String endpoint, String method, JsonObject body) throws IOException {
+        String secret = (String) SkriptTebex.getPlugin(SkriptTebex.class).getConfig().get("tebex-secret");
+        URL url = new URL("https://plugin.tebex.io/" + endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(method);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("X-Tebex-Secret", secret);
+        connection.setDoOutput(true);
+        if (body != null) {
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = body.toString().getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+        }
+        if (connection.getResponseCode() == 200 || connection.getResponseCode() == 204 || connection.getResponseCode() == 201) {
+            InputStream stream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            return JsonParser.parseString(response.toString()).getAsJsonObject();
+        }
+        return null;
+    }
+
 }
